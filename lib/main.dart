4edfa7e58aeb4f1c8b8a53c23458e7603c5180c3
Alexandre <alexandresilva58@gmail.com>
2021-08-30 23:12:49 -0300

    (chore) Add theme

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Meu Primeiro App"),
+        title: Text("Meu Primeiro App Flutter"),
       ),
       body: Center(
           child: Text(
